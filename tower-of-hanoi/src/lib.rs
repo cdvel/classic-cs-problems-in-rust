0c6e0e6f12575dad3841961259cdec9611bf3625
Cesar D. Velandia <cesar@velandia.co>
2020-06-08 16:03:26 +0800

    Run fmt and clippy

--- tower-of-hanoi/src/lib.rs
+++ tower-of-hanoi/src/lib.rs
@@ -1,30 +1,27 @@
 pub fn hanoi(origin: &mut Vec<i32>, target: &mut std::vec::Vec<i32>, temp: &mut Vec<i32>, n: i32) {
-
-    if n == 1{
+    if n == 1 {
         target.push(origin.pop().unwrap());
-    }else{
+    } else {
         hanoi(origin, temp, target, n - 1);
         hanoi(origin, target, temp, 1);
         hanoi(temp, target, origin, n - 1);
     }
 }
 
-pub fn hanoi_target(n: i32) -> std::vec::Vec<i32>{
-    
+pub fn hanoi_target(n: i32) -> std::vec::Vec<i32> {
     let (mut a, mut b, mut c) = init(n);
     hanoi(&mut a, &mut b, &mut c, n);
     b.to_vec()
 }
 
-pub fn init(n: i32) -> (Vec<i32>, Vec<i32>, Vec<i32>){
+pub fn init(n: i32) -> (Vec<i32>, Vec<i32>, Vec<i32>) {
     let mut a: Vec<i32> = Vec::new();
     let b: Vec<i32> = Vec::new();
     let c: Vec<i32> = Vec::new();
-    
-    for i in 1..n+1{
+
+    for i in 1..n + 1 {
         a.push(i);
     }
 
     (a, b, c)
-
 }
